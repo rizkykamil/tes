ddc53abdbc4a85d7303ccad2cadeb977f3db21e2
rizkykamil <54827655+rizkykamil@users.noreply.github.com>
2022-02-26 22:08:48 +0700

    test.js

--- test.js
+++ test.js
@@ -3,23 +3,14 @@ const sentences = [
   'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
   'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
 ];
-
+// Find maximum number of words from given sentences.
 function result(sentences) {
-  let result = [];
-  for (let i = 0; i < sentences.length; i++) {
-    let sentence = sentences[i];
-    let words = sentence.split(' ');
-    let wordCount = words.length;
-    let wordLength = words.reduce((acc, cur) => {
-      return acc + cur.length;
-    }, 0);
-    result.push({
-      sentence,
-      wordCount,
-      wordLength,
-    });
-  }
-  return result;
+    let result = [];
+    for (let i = 0; i < sentences.length; i++) {
+        let words = sentences[i].split(' ');
+        result.push(words.length);
+    }
+    return Math.max(...result);
 }
 
 console.log(result(sentences));
